5914271cbc777bdeab4cf36082168e2c38139c45
mimmo <mimmo@mimmo-Lenovo-ideapad-300-15ISK>
2020-01-26 21:23:16 +0100

    nuova classe animale

---
+++ src/testGit/Animale.java
@@ -0,0 +1,29 @@
+package testGit;
+
+public class Animale {
+	
+	String nome; 
+	String tipo ; 
+	Integer eta;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public Integer getEta() {
+		return eta;
+	}
+	public void setEta(Integer eta) {
+		this.eta = eta;
+	} 
+	
+}
