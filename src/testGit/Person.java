3263465eabede692702b82fa413a4714fbe0b1aa
mimmo <mimmo@mimmo-Lenovo-ideapad-300-15ISK>
2020-01-26 20:57:18 +0100

    hh

---
+++ src/testGit/Person.java
@@ -0,0 +1,33 @@
+package testGit;
+
+public class Person {
+
+	String nome;
+	String cognome;
+	int eta;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public int getEta() {
+		return eta;
+	}
+
+	public void setEta(int eta) {
+		this.eta = eta;
+	}
+
+}
